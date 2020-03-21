6569e1c80cfdff82820c9f03a072686a5e715eb7
unknown <css.raja>
2020-03-21 16:57:38 +0530

    Updates

---
+++ precache-manifest.d6bc6ea64f7a9eaf178e822da445622d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1e4de2e8a9e0639e70c1590040c485d",
+    "url": "/weather-details/index.html"
+  },
+  {
+    "revision": "7724503cb94fcd74accb",
+    "url": "/weather-details/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6d02a4318d4ff85d7fb1",
+    "url": "/weather-details/static/js/2.682afd33.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-details/static/js/2.682afd33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7724503cb94fcd74accb",
+    "url": "/weather-details/static/js/main.b22e6daf.chunk.js"
+  },
+  {
+    "revision": "93bf49e253eb082bbd3f",
+    "url": "/weather-details/static/js/runtime-main.6b7d81c6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/weather-details/static/media/logo.5d5d9eef.svg"
+  }
+]);
